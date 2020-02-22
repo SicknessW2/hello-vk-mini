efcff1b2454854f2917d069fc594ae8e48fc3312
SicknessW2 <sosiska74vip@gmail.com>
2020-02-23 01:09:04 +0500

    Updates

---
+++ precache-manifest.f90b8e8df98defde0815c645c16267a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb75658b3258001122e50671d760a34",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c9a27f422ebcfd10a74",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "df89b1418c9672bc5e35",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7c9a27f422ebcfd10a74",
+    "url": "./static/js/2.10918f7a.chunk.js"
+  },
+  {
+    "revision": "df89b1418c9672bc5e35",
+    "url": "./static/js/main.3fc9f229.chunk.js"
+  },
+  {
+    "revision": "887d7230c8fb8fe00f5a",
+    "url": "./static/js/runtime-main.0215ed67.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
